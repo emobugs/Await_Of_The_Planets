0992cd143b97497479132bda5a864a6b7e23943a
Hristiyan Ivanov <civanov.me@gmail.com>
2021-09-16 18:47:01 +0300

    feat: add boilerplate code

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,5 @@
 import EventEmitter from "eventemitter3";
+import image from "../images/planet.svg";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -10,11 +11,37 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.innerHTML = this._render({
+      name: "Placeholder",
+      terrain: "placeholder",
+      population: 0,
     });
 
+    document.body.querySelector(".main").appendChild(box);
+
     this.emit(Application.events.READY);
+  }
+
+  _render({ name, terrain, population }) {
+    return `
+<article class="media">
+  <div class="media-left">
+    <figure class="image is-64x64">
+      <img src="${image}" alt="planet">
+    </figure>
+  </div>
+  <div class="media-content">
+    <div class="content">
+    <h4>${name}</h4>
+      <p>
+        <span class="tag">${terrain}</span> <span class="tag">${population}</span>
+        <br>
+      </p>
+    </div>
+  </div>
+</article>
+    `;
   }
 }
